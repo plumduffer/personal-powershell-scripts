8afb4608657edc5b3f216704db5aaac7db4bff5c
plumduffer <85471640+plumduffer@users.noreply.github.com>
2023-05-13 19:42:33 +1200

    Update ffmpeg-helpers.ps1

--- ffmpeg-helpers.ps1
+++ ffmpeg-helpers.ps1
@@ -11,3 +11,17 @@ function resize_func {
 }
 
 New-Alias -Name resize -Value resize_func
+
+function darken_func {
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]$File,
+    [int]$Sat = 3,
+    [float]$Bright = 0.1
+  )
+  $datetime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+  $output = $File -replace "\.(?=[^.]*$)", "_sat${Sat}_bright${Bright}_${datetime}."
+  ffmpeg -i $File -vf "eq=saturation=${Sat}:brightness=-${Bright},split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" $output
+}
+
+New-Alias -Name darken -Value darken_func
